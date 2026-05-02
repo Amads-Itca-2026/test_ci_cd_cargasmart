6ae697c1f4dc0683921a3d1697439e5cbe9d09a9
ingcrux <gcruz@itca.edu.sv>
2026-05-02 15:37:08 -0600

    Correcciones en operador math

--- math.js
+++ math.js
@@ -4,7 +4,7 @@
 function calcularCostoTotal(rutaA, rutaB) {
     const costoRutaA = rutaA.reduce((total, costo) => total + costo, 0);
     const costoRutaB = rutaB.reduce((total, costo) => total + costo, 0);
-    return costoRutaA - costoRutaB;
+    return costoRutaA + costoRutaB;
 }
 
 module.exports = {
